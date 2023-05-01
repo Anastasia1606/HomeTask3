849a222bcd90bd8ea6847153df9f010942b50ae0
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-01 21:53:17 +0500

    add Task1_Palindrome

---
+++ Task1_Palindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+if (number < 10000 || number >= 100000)
+{
+  Console.WriteLine("Вы ввели не пятизначное число, пожалуйста, повторите ввод");
+  return;
+}
+int a = number / 10000;
+int b = number / 1000 % 10;
+int c = number / 100 % 10;
+int d = number / 10 % 10;
+int e = number % 10;
+if (a==e&&b==d)
+{
+  Console.WriteLine("Число является палиндромом");
+}
+else
+{
+ Console.WriteLine("Число не является палиндромом");
+}
